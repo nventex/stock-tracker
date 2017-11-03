1c1bd5a49a6b319a10fa984bbdf8b7bbb9d7771c
Nventex <nventex@gmail.com>
2017-11-03 10:18:59 -0500

    update

--- commands/getStocksCommand.js
+++ commands/getStocksCommand.js
@@ -26,63 +26,63 @@ var async = require("async");
 //   };
 // }
 
-// var handle = function(callBack) {
-
-//     var calls = [];
-
-//     Stock.find({}, function(err, stocks) {
-
-//         stocks.forEach(function(stock) {
-            
-//             calls.push(function(asyncCallback) {
-//                 stockService.getCurrentStockPrice(stock.symbol, function(stockPrice) {
-//                     stock.price = stockPrice;
-//                     Calculator.calculateTransactions(stock);
-
-//                     if (err) {
-//                         return asyncCallback(err);
-//                     }
-
-//                     asyncCallback(null, stockPrice);
-
-//                 });
-//             });
-//         });
-
-//         async.parallel(calls, function(error, result) {
-//             callBack(stocks);
-//         });
-    
-//     }).sort([["symbol", "ascending"]]);
-// }
-
 var handle = function(callBack) {
 
     var calls = [];
 
     Stock.find({}, function(err, stocks) {
 
-        var symbols = Array.prototype.map.call(stocks, function(stock) {
-            return stock.symbol;
-        }).join("%22%2C%22");
-
-        stockService.getCurrentStockPrices(symbols, function(results) {
+        stocks.forEach(function(stock) {
             
-            stocks.forEach(function(stock) {
-                var stockResult = results.find(function(result) {
-                    return result.Symbol == stock.symbol;
+            calls.push(function(asyncCallback) {
+                stockService.getCurrentStockPrice(stock.symbol, function(stockPrice) {
+                    stock.price = stockPrice;
+                    Calculator.calculateTransactions(stock);
+
+                    if (err) {
+                        return asyncCallback(err);
+                    }
+
+                    asyncCallback(null, stockPrice);
+
                 });
-                
-                stock.price = stockResult.LastTradePriceOnly;
-                Calculator.calculateTransactions(stock);
-
             });
-
-            callBack(stocks);
-
         });
 
+        async.parallel(calls, function(error, result) {
+            callBack(stocks);
+        });
+    
     }).sort([["symbol", "ascending"]]);
 }
+
+// var handle = function(callBack) {
+
+//     var calls = [];
+
+//     Stock.find({}, function(err, stocks) {
+
+//         var symbols = Array.prototype.map.call(stocks, function(stock) {
+//             return stock.symbol;
+//         }).join("%22%2C%22");
+
+//         stockService.getCurrentStockPrices(symbols, function(results) {
+            
+//             stocks.forEach(function(stock) {
+//                 var stockResult = results.find(function(result) {
+//                     return result.Symbol == stock.symbol;
+//                 });
+                
+//                 stock.price = stockResult.LastTradePriceOnly;
+//                 Calculator.calculateTransactions(stock);
+
+//             });
+
+//             callBack(stocks);
+
+//         });
+
+//     }).sort([["symbol", "ascending"]]);
+// }
 
 module.exports = handle;
--- services/stockService.js
+++ services/stockService.js
@@ -4,13 +4,17 @@ var client = new Client();
 
 var stockService = {
     getCurrentStockPrice: function(symbol, callBack) {
-        var url = "https://query.yahooapis.com/v1/public/yql?q=select%20Symbol%2C%20LastTradePriceOnly%20from%20yahoo.finance.quote%20where%20symbol%20%3D%20%22" + symbol + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        var url = "https://finance.google.com/finance/getprices?p=2d&f=c,o&q=" + symbol;
         client.get(url, function(data, response) {
             
-            // var responseData = data.toString("utf8");
-            // var jsonData = JSON.parse(responseData);
+            var responseData = data.toString("utf8");
+            var resp = String(responseData);
+            var costLine = resp.replace(/^[a-zA-Z]+.+|\n$/gm, "");
+            costLine = costLine.replace(/\n/gm, "");
 
-            callBack(data.query.results.quote.LastTradePriceOnly);
+            var prices = costLine.split(",");
+
+            callBack(prices[0]);
         });
     },
 
